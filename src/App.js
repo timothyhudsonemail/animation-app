85df3a05aa45659d65d497621341eaa05b52d83c
timothyhudsonemail <60154902+timothyhudsonemail@users.noreply.github.com>
2021-04-06 10:56:24 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css'
 
 export default class App extends Component {
 
+
+
   constructor(props) {
     super(props)
     this.state = {
@@ -14,105 +16,122 @@ export default class App extends Component {
     }
   }
 
+
+  getImages = () => {
+    
+    this.setState({isLoading: true})
+
+    fetch("https://server-v1-2021.herokuapp.com/a")
+    .then(res => res.json())
+    .then(
+      (result) => {
+          this.setState({
+          img1: result[0].img1,
+          img2: result[0].img2,
+          img3: result[0].img3,
+          img4: result[0].img4,
+          img5: result[0].img5,
+          img6: result[0].img6,
+          img7: result[0].img7,
+          img8: result[0].img8,
+          img9: result[0].img9,
+          img10: result[0].img10,
+          img11: result[0].img11,
+          img12: result[0].img12,
+          click: true,
+          activeImage: this.state.img1,
+          isLoading: false,
+          click2:true,
+          })
+        })
+  }
+
+
+
+    
+  animateImages = () => {
+    if (this.state.activeImage === this.state.img1) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img2})
+      },366)
+      }
+    else if (this.state.activeImage === this.state.img2) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img3})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img3) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img4})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img4) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img5})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img5) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img6})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img6) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img7})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img7) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img8})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img8) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img9})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img9) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img10})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img10) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img11})
+      },366)
+    }
+    else if (this.state.activeImage === this.state.img11) {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img12})
+      },366)
+    }
+    else {
+      setTimeout(() => {
+        this.setState({activeImage: this.state.img1})
+      }, 366)
+    }
+    setTimeout(this.animateImages, 366)
+  }
+
+  open = () => {
+    this.setState({click2: true})
+  }
+
+
+
+
+  
+  componentDidMount(){
+    this.getImages();
+    
+    this.animateImages();
+  }
+ 
+
   render() {
 
-    const getImages = (e) => {
-      e.preventDefault()
-      this.setState({isLoading: true})
-
-      fetch("https://server-v1-2021.herokuapp.com/a")
-      .then(res => res.json())
-      .then(
-        (result) => {
-            this.setState({
-            img1: result[0].img1,
-            img2: result[0].img2,
-            img3: result[0].img3,
-            img4: result[0].img4,
-            img5: result[0].img5,
-            img6: result[0].img6,
-            img7: result[0].img7,
-            img8: result[0].img8,
-            img9: result[0].img9,
-            img10: result[0].img10,
-            img11: result[0].img11,
-            img12: result[0].img12,
-            click: true,
-            activeImage: this.state.img1,
-            isLoading: false
-            })
-          })
-    }
 
     
-    
-    const animateImages = () => {
-      if (this.state.activeImage === this.state.img1) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img2})
-        },366)
-        }
-      else if (this.state.activeImage === this.state.img2) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img3})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img3) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img4})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img4) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img5})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img5) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img6})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img6) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img7})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img7) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img8})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img8) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img9})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img9) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img10})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img10) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img11})
-        },366)
-      }
-      else if (this.state.activeImage === this.state.img11) {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img12})
-        },366)
-      }
-      else {
-        setTimeout(() => {
-          this.setState({activeImage: this.state.img1})
-        }, 366)
-      }
-      setTimeout(animateImages, 0)
-    }
-
-    const open = () => {
-      this.setState({click2: true})
-    }
 
     const changeBackground = () => {
       if(this.state.bg === "gray")
@@ -130,39 +149,40 @@ export default class App extends Component {
       }
     }
 
+    
+
     return (
       <div className="App">
         <h1 style={{fontFamily:"Varta"}}>Animation Study</h1>
-        <div style={{display:"flex",flexDirection:"row",height:"40px"}}>
+        <div style={{display:"flex",
+                    flexDirection:"row", 
+                    justifyContent:"center", 
+                    height:"40px"}}>
           
-          <div onClick={navButtons} id="appTitle" className={this.state.chosenSection ? "" : "selected"} style={{width:"50%"}}>
-          <p style={{cursor:"pointer"}}>App</p>
+          <div className={this.state.chosenSection ? "p-container" : "selected p-container"}
+               onClick={navButtons}>
+          <p>App</p>
           </div>
           
-          <div onClick={navButtons} id="aboutTitle" className={this.state.chosenSection ? "selected" : ""} style={{width:"50%"}}>
-          <p style={{cursor:"pointer"}}>About</p>
+          <div className={this.state.chosenSection ? "selected p-container" : "p-container"}
+               onClick={navButtons}>
+          <p>
+          About</p>
           </div>
         </div> 
 
         <div id="application" style={{display:this.state.chosenSection ? 'none' : 'block' }}>
 
-        <br />
+        <br /><br />
 
-        <button onClick={getImages}>open</button>
         
-        <br />
-        <br />
 
         <div style={{display:this.state.isLoading ? 'block' : 'none'}}>
           <p>loading...</p>
         </div>
         
         <div style={{display: this.state.click ? 'block' : 'none' }}>
-          <button onClick={() => {
-              open()
-              animateImages()
-              }}>start
-          </button>
+          
 
           <div id="bg" 
             className="background" 
@@ -172,7 +192,9 @@ export default class App extends Component {
                    className="super"
                    src= {this.state.activeImage}
               />
-              <button onClick={changeBackground}>change background</button>
+              <button className="button2" 
+                      onClick={changeBackground}>change background</button>
+              
               <div style={{height:"120px"}}></div>
             
             </div>
@@ -181,7 +203,8 @@ export default class App extends Component {
 
       <div id="about" 
            style={{display:this.state.chosenSection ? 'block' : 'none' }}>
-        <br />
+        <br /><br />
+
         <div className="about-style">
           <div>
             <p className="p-style">
@@ -191,7 +214,7 @@ export default class App extends Component {
         
           <div>
             <p className="p-style">
-            Technical info: This application is built using React. Front end hosted on Vercel (deployed via github), server built from scratch and hosted on Heroku. PSQL database stores .png animation file info, hosted on aws s3.
+            This application is built using React. The front end is hosted on Vercel/Github, the server is built from scratch and hosted on Heroku, and a handmade PSQL database stores .png animation file info; .png files hosted on AWS s3. 
             </p>
           </div>
         </div>
